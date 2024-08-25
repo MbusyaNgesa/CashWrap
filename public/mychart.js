b2263b1df73c524a21e66b7d2d1d788b9b6202b4
MbusyaNgesa <theengesa@gmail.com>
2024-08-25 18:58:41 +0300

    Chart JS

---
+++ public/mychart.js
@@ -0,0 +1,27 @@
+const ctx = document.getElementById("myChart");
+
+new Chart(ctx, {
+  type: "doughnut",
+  data: {
+    labels: ["  Rent", "Travels", "Food"],
+    datasets: [
+      {
+        label: "My First Dataset",
+        data: [2500, 5500, 200],
+        backgroundColor: [
+          "rgb(255, 99, 132)",
+          "rgb(54, 162, 235)",
+          "rgb(255, 205, 86)",
+        ],
+        hoverOffset: 4,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
