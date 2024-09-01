6be851af77b32b7c8fc51dd247f865300ffab7c5
MbusyaNgesa <theengesa@gmail.com>
2024-09-02 00:05:31 +0300

    Appending to Card

--- public/script.js
+++ public/script.js
@@ -3,6 +3,10 @@ const cancel = document.querySelector(".cancel");
 const budget = document.querySelector(".budget");
 const modal = document.querySelector(".modal");
 const closeModal = document.getElementById("closeModal");
+const name = document.getElementById("name");
+const addBudget = document.querySelector(".addBudget");
+const budgetCard = document.querySelector(".budgetCard");
+console.log(budgetCard);
 
 filter.addEventListener("click", sidebar);
 cancel.addEventListener("click", removeSidebar);
@@ -19,28 +23,31 @@ function removeSidebar(e) {
 budget.addEventListener("click", () => {
   const card = document.getElementById("crud-modal");
   card.style.display = "block";
-  console.log(card);
 });
 
 //Close Modal
 closeModal.addEventListener("click", () => {
   const closeCard = document.getElementById("crud-modal");
   closeCard.style.display = "none";
-  console.log(closeCard);
 });
 
-// Function to show the modal
-// function showModal() {
-//   modal.style.display = "block";
-// }
+//Modal Value
+addBudget.addEventListener("click", function budget(e) {
+  e.preventDefault();
+  let newName = document.getElementById("name").value;
+  let newPrice = document.getElementById("price").value;
+  console.log(newName);
+  console.log(newPrice);
 
-// function hideModal() {
-//   modal.style.display = "none";
-// }
+  let element = document.createElement("div");
+  element.className = "flex space-x-3 mt-6 bg-purp rounded-2xl p-2 text-center";
 
-// budget.addEventListener("click", showModal);
-// window.onclick = function (e) {
-//   if (e.target == modal) {
-//     modal.style.display = "none";
-//   }
-// };
+  let image = document.createElement("img");
+  image.src = "img/Travels.jpg";
+  image.alt = "invalid";
+  image.className = "rounded-full h-7 w-7";
+  element.appendChild(image);
+  console.log(element);
+
+  budgetCard.appendChild(element);
+});
