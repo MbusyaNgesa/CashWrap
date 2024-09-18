c27cff791afa4c037ba5753d696eed9f1f9add8a
MbusyaNgesa <theengesa@gmail.com>
2024-09-18 12:17:05 +0300

    MongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 import express from "express";
+import { connectDB } from "./db/connectDB.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
-  res.send("Hello Ngesa ");
+  res.send("Hello Ngesa");
 });
 
 app.listen(PORT, () => {
-  console.log("Server is operational");
+  connectDB();
+  console.log("Server is operational:", PORT);
 });
