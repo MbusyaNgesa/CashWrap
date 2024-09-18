6a16f46a258faaa23988694e8e7169da788f24d7
MbusyaNgesa <theengesa@gmail.com>
2024-09-18 15:38:49 +0300

    Database Setup

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,9 @@ import authroutes from "./routes/auth.routes.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
+
+app.use(express.json());
 
 app.use("/api/auth", authroutes);
 
